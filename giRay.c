386b770b5b5d23ac19e2ee72232ca8529e9c3784
gibocx <jogi8230@gmail.com>
2020-12-23 20:46:03 +0100

    yeetss recusrionss

--- giRay.c
+++ giRay.c
@@ -1,76 +1,75 @@
 #include <time.h>
+#include <stdio.h>
+//#include <termios.h>
 
-int getFunnyValue(const int value)
-{
-  static short numbers[] = {'{o',0x1749,0x73E7,0x72CF,0x5BC9,0x79CF,0X79EF,0x7292,0x7BEF,0x7BC9};
-//   *(&value) = *(&value) + 1;
-   return numbers[value];
-}
-
-void print(char c)
-{printf("%c",c);}
+//todo render each number independentply 
 
 void render(int values[])
 {
-  int i,k,l,val,q,o;
+  int j,i,k,l,val,q,o;
 
-for(o=0;o++<3;print('\n'));
-
-  for(int j = 0;j < 5;j++){
+for(o=0;o++<3;main(4,'\n'));
+  for(j = 0;j < 5;j++){
     for(l = 0; l < 3;l++){
-        for(o=0;o++<4;print(' '));
+
+        for(o=0;o++<4;main(4,' '));
         for(k = 0;k < 6;k++){
             for(i = 0;i < 3; i++){
 
-            val = getFunnyValue(values[k]);
+	    val = main(3,values[k]);
 
             for(q=0;q<3;q++){
             if(val & ((1 << 14) >> ((j*3)+i)))
-            {print('#');}else{print(' ');}
+            {main(4,'#');}else{main(4,' ');}
           }}
 
           if((k % 2) == 1)
           {
-                for(o=0;o++<2;print(' '));
+                for(o=0;o++<2;main(4,' '));
 
                 if((j % 2) == 0)
                 {
-                    print(' ');
+                    main(4,' ');
                 }
                 else
                 {
                     if(k != 5)
-                        print('#');
+                        main(4,'#');
+
                 }
           }
-      for(o=0;o++<2;print(' '));
+      for(o=0;o++<2;main(4,' '));
     }
-    print('\n');
+    main(4,'\n');
     }
 
   }
-  for(o=0;o++<3;print('\n'));
+  for(o=0;o++<3;main(4,'\n'));
 }
 
-int main(int argc, char *argv[])
+short val[] = {'{o',0x1749,0x73E7,0x72CF,0x5BC9,0x79CF,0X79EF,0x7292,0x7BEF,0x7BC9};
+
+int main(int argc, char **argv)
 {
-//  int i;
-//  for(i=1;i < argc;i++)
-//  {
-//    switch(argv[i])
-//    {
-//      case "time":
-       // time_t t = time(0);
-       // struct tm* tm= localtime(&t);
-        int sec = -1;
+       if(argc > 1)
+       {
+// pointer magic
+          if(!(argc-3)){return val[(int)argv];}
+          if(!(argc-4)){putchar(argv);return 0;}
+
+       }
+       else
+       {
+       int sec = -1;
         int array[6];
 
         while(1){
-time_t   t = time(0);
-struct tm*   tm = localtime(&t);
+	time_t   t = time(0);
+	struct tm*   tm = localtime(&t);
         if(sec != tm->tm_sec)
         {
             sec = tm->tm_sec;
+// compact this shit a lot
             array[0] = tm->tm_hour / 10;
             array[1] = tm->tm_hour % 10;
             array[2] = tm->tm_min / 10;
@@ -79,7 +78,8 @@ struct tm*   tm = localtime(&t);
             array[5] = tm->tm_sec % 10;
 
             render(array);
+
         }
 }
 }
-
+}
---
+++ run.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+clear
+rm ./giRazzz
+gcc ./giRay.c -o ./giRazzz -Wall -O3 -g
+./giRazzz
